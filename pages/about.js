f4c40ca2d80b5f02d60135df48348b637720d142
Thomas Greco <thomasjosephgreco@yahoo.com>
2017-12-12 13:14:31 -0500

    pushing final code for video 1

---
+++ pages/about.js
@@ -0,0 +1,6 @@
+const About = ({ title = 'Hello from the about route!' }) => 
+<div>
+    <h2>{title}</h2>
+</div>;
+
+export default About;
--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,6 @@
-import Link from 'next/link'
-import 'isomorphic-fetch';
-import withStyles from '../shared/MUI/withMUI';
-import Header from '../components/header';
-import { Card, CardHeader, CardText } from 'material-ui/Card'
-import RaisedButton from 'material-ui/RaisedButton'
+const Index = ({ title = 'Hello from next.js' }) => 
+    <div>
+        <h2>{title}</h2>
+    </div>;
 
-const styles = {
-  cardStyles: {
-    margin: 10,
-  }
-}
-
-const Page = ({posts}) =>
-  <div>
-  <Header />
-    {posts.map(x =>
-      <Card key={x.id} style={styles.cardStyles}>
-      <CardHeader title={x.title}  />
-       <CardText>
-        <RaisedButton primary={true} fullWidth={true}>
-        <Link prefetch href={`/post?id=${x.id}`} as={`/blog/${x.id}`}>
-          <a>
-            Click to Read This Post
-          </a>
-        </Link>
-        </RaisedButton>
-        </CardText>
-      </Card>
-    )}
-  </div>
-
-
-Page.getInitialProps = async () => {
-    const response = await fetch(`https://www.googleapis.com/blogger/v3/blogs/4789269094064278868/posts?key=${process.env.BLOGGER_API_KEY}`)
-    const data = await response.json()
-    return {posts: data.items};
-  }
-
-export default withStyles(Page);
-  
+export default Index;
