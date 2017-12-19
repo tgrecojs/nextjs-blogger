ee736eabcf89512e3a7f751ca9e1d2f445685b4f
Thomas Greco <thomasjosephgreco@yahoo.com>
2017-12-18 23:23:11 -0500

    pushing final code for video 4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
-
 require('dotenv').config();
+
 module.exports = {
   webpack: config => {
     config.plugins.push(
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,28 @@
 import Header from '../components/header';
 import withMui from '../shared/MUI/withMUI';
+import 'isomorphic-fetch';
 import { Card, CardHeader, CardText } from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
-import 'isomorphic-fetch';
 
 const Index = ({ posts }) => 
     <div>
     <Header />
-       {
-           posts.map(x =>
-            <Card key={x.id}>
+      {
+          posts.map(x => 
+            <Card key={x.id}> 
                 <CardHeader title={x.title} />
                 <CardText>
-                    <RaisedButton label="Click here to read post!" />
+                    <RaisedButton label="Click to view post!" fullWidth={true} primary={true} />
                 </CardText>
             </Card>
         )
-       }
+      }
     </div>;
 
 Index.getInitialProps = async () => {
-    const res = await fetch(`${process.env.BLOGGER_URL}?key=${process.env.API_KEY}`);
-    const data = await res.json();
-    return { posts: data.items };
+    const response = await fetch(`${process.env.BLOGGER_URL}?key=${process.env.API_KEY}`);
+    const data = await response.json();
+    return { posts: data.items }
 }
 
 export default withMui(Index);
