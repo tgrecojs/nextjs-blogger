c42d7e81e4dfe2731e76f74af198357b72eb703a
Thomas Greco <thomasjosephgreco@yahoo.com>
2018-01-21 19:36:35 -0500

    pushing code for video 5

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const next = require('next')
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    const server = express();
+
+    server.get('/blog', (req, res) => app.render(req, res, '/'));
+
+    server.get('/', (req, res) => res.redirect(301, '/blog'));
+
+    server.get('/*', (req, res) => handle(req, res));
+
+    server.listen(port, err => {
+        if(err) throw err
+        console.log(`> Read on http://localhost:${port}`);
+    })
+});
