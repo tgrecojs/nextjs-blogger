3a118de7bac02eb9b37912c635bb1b112cb0104c
Thomas Greco <thomasjosephgreco@yahoo.com>
2018-01-21 19:30:01 -0500

    pushing code for part2 final

--- components/header.js
+++
@@ -1,6 +0,0 @@
-import AppBar from 'material-ui/AppBar';
-
-const Header = ({ title = 'Next.js blogging application'}) =>
-    <AppBar title={title} showMenuIconButton={false} />
-
-export default Header;
